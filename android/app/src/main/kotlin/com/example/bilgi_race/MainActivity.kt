6456acb1663e9db4f7034e2c9c9fc4ce2c3a24a2
umut sıralı <umut2001sirali@gmail.com>
2023-07-06 16:42:41 +0300

    started

---
+++ android/app/src/main/kotlin/com/example/bilgi_race/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bilgi_race
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'constants.dart';
+
+void main() => runApp(BilgiTesti());
+
+class BilgiTesti extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.indigo[700],
+            body: SafeArea(
+                child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: SoruSayfasi(),
+            ))));
+  }
+}
+
+class SoruSayfasi extends StatefulWidget {
+  @override
+  _SoruSayfasiState createState() => _SoruSayfasiState();
+}
+
+class _SoruSayfasiState extends State<SoruSayfasi> {
+  List<Widget> secimler = [
+    smile,
+    smile,
+    bad,
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'Bilgi Testi Soruları',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: secimler,
+        ),
+        Expanded(
+          flex: 1,
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6.0),
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: ElevatedButton(
+                          child: Icon(
+                            Icons.thumb_down,
+                            size: 30.0,
+                          ),
+                          onPressed: () {},
+                        ))),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        child: ElevatedButton(
+                          child: Icon(Icons.thumb_up, size: 30.0),
+                          onPressed: () {},
+                        ))),
+              ])),
+        )
+      ],
+    );
+  }
+}
+
+// Icon smile = Icon(
+//   Icons.mood,
+//   color: Colors.red,
+// );
+// Icon bad = Icon(
+//   Icons.mood_bad,
+//   color: Colors.deepOrange,
+// );
