84a3ee805518c692c9e3320c0aab7f5585765362
umut sıralı <umut2001sirali@gmail.com>
2023-07-07 11:31:03 +0300

    questions have added

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+Icon smile = Icon(
+  Icons.mood,
+  color: Colors.red,
+);
+Icon bad = Icon(
+  Icons.mood_bad,
+  color: Colors.deepOrange,
+);
